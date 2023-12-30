18383a1ec599101ad674d84defbd6a2158258fc6
Jeffrey2081 <87138660+Jeffrey2081@users.noreply.github.com>
2023-12-30 14:19:28 +0530

    Summary

--- postinstall.sh
+++ postinstall.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 set -e
 # Installing Basic Packages
-sudo pacman -Syu --needed --noconfirm archlinux-keyring pacman-contrib terminus-font base-devel rsync reflector
+sudo pacman -Syu --needed --noconfirm curl terminus-font base-devel rsync reflector networkmanager git neofetch nano vim 
 sudo setfont ter-v22b
-iso=$(curl -4 ifconfig.co/country-iso)
 
 # Enable parellel Downloads
 sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 # Setting Mirrors for fast downloads 
+iso=$(curl -4 ifconfig.co/country-iso)
 sudo reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Installing yay aur helper
@@ -25,6 +25,7 @@ sudo mkdir /home/$USERNAME/.fonts
 yay -S nerd-fonts-complete-starship noto-fonts-emoji netctl variety feh ttf-font-awesome jq polybar redshift sddm nano vim sxhkd neofetch psmisc lxappearance papirus-icon-theme noto-fonts-emoji bspwm kitty polybar picom thunar nitrogen xorg unzip yad wget pulseaudio pavucontrol qt5-quickcontrols qt5-quickcontrols2 qt5-svg rofi lxpolkit-git ttf-font-awesome brave-bin --noconfirm
 
 # Theming
+git clone https://github.com/Jeffrey2081/jeff-arch
 cd ~/jeff-arch || exit
 sudo cp -r bg.jpg /home/$USER/Pictures/bg.jpg
 sudo cp -r dotconfig/* /home/$USER/.config/
