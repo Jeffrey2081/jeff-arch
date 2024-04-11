dc5e497e67de4958bacd7a6741949aac093bbfe1
Jeffrey2081 <87138660+Jeffrey2081@users.noreply.github.com>
2024-04-11 18:38:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,44 +1,89 @@
-#! /bin/sh
-sudo pacman -Syu --needed --noconfirm curl terminus-font base-devel rsync reflector networkmanager git neofetch nano vim 
-sudo setfont ter-v22b
-echo "Enter your username:  "
-read USERNAME
-set -e
-# Installing yay aur helper
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -eq 0 ]]; then
+  echo "Please do not run this script as root, run it as a regular user." 2>&1
+  exit 1
+fi
+# Enable multilib repository
+sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+# Enable parallel downloading
+sudo sed -i 's/#CleanMethod = KeepInstalled/CleanMethod = KeepInstalled/' /etc/yay.conf
+
+# Update system and install essential tools
+sudo pacman -Syu --noconfirm base-devel git
+
+# Install yay AUR Helper
 git clone https://aur.archlinux.org/yay-git.git
-cd yay-git
-makepkg -si
+cd yay || exit
+makepkg -si --noconfirm
 cd ..
+rm -rf yay-git
 
-# Creating a few directories
-mkdir /home/$USERNAME/Pictures
-mkdir /home/$USERNAME/.config
-mkdir /home/$USERNAME/.fonts
-mkdir /home/$USERNAME/.local/
-mkdir /home/$USERNAME/.local/share/
-mkdir /home/$USERNAME/.local/share/icons
-# Installing the dependencies and the window manager bspwm
-yay -Syu --noconfirm nerd-fonts-complete-starship noto-fonts-emoji netctl variety feh ttf-font-awesome jq polybar redshift sddm nano vim sxhkd neofetch psmisc lxappearance papirus-icon-theme noto-fonts-emoji bspwm kitty polybar picom thunar nitrogen xorg unzip yad wget pulseaudio pavucontrol qt5-quickcontrols qt5-quickcontrols2 qt5-svg rofi lxpolkit-git ttf-font-awesome brave-bin --noconfirm
+# Making .config and Moving config files and background to Pictures
+username=$(id -u -n 1000)
+builddir=$(pwd)
+mkdir -p "/home/$username/.config"
+mkdir -p "/home/$username/.fonts"
+mkdir -p "/home/$username/Pictures"
+mkdir -p /usr/share/sddm/themes
+cp .Xresources "/home/$username"
+cp .Xnord "/home/$username"
+cp -R dotconfig/* "/home/$username/.config/"
+cp bg.jpg "/home/$username/Pictures/"
+mv user-dirs.dirs "/home/$username/.config"
+sudo chown -R "$username:$username" "/home/$username"
+tar -xzvf sugar-candy.tar.gz -C /usr/share/sddm/themes
+mv "/home/$username/.config/sddm.conf" /etc/sddm.conf
 
-# Theming
-cd ~/jeff-arch || exit
-cp -r bg.jpg /home/$USERNAME/Pictures/bg.jpg
-cp -r dotconfig/* /home/$USERNAME/.config/
-git clone https://github.com/Jeffrey2081/fonts
-cp -r fonts/dotfonts/* /home/$USERNAME/.fonts/
+# Installing sugar-candy dependencies
+yay -S --noconfirm qt5-svg qt5-declarative
+# Installing Essential Programs 
+yay -S --noconfirm feh bspwm sxhkd kitty rofi polybar picom thunar lxpolkit xorg-xbacklight unzip yad wget pulseaudio pavucontrol
+# Installing Other less important Programs
+yay -S --noconfirm neofetch flameshot psmisc mangohud vim papirus-icon-theme sddm
 
+# Download Nordic Theme
+cd /usr/share/themes/ || exit
+git clone https://github.com/EliverLara/Nordic.git
 
-git clone https://github.com/alvatip/Nordzy-cursors
-
-cp -r Nordzy-cursors/  ~/.local/share/icons
+# Installing fonts
+cd "$builddir" || exit
+yay -S --noconfirm ttf-font-awesome
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-cp -r /home/$USERNAME/jeff-arch/fonts/dotfonts/fontawesome/otfs/* /home/$USERNAME/.fonts/
-unzip FiraCode.zip -d /home/$USERNAME/.fonts/
+unzip FiraCode.zip -d "/home/$username/.fonts"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
- unzip Meslo.zip -d /home/$USERNAME/.fonts/
-sudo chown $USERNAME:$USERNAME /home/$USERNAME/*
-cd /usr/share/themes/ 
-sudo git clone https://github.com/EliverLara/Nordic.git
-sudo systemctl enable sddm
-sudo fc-cache -vf
+unzip Meslo.zip -d "/home/$username/.fonts"
+mv dotfonts/fontawesome/otfs/*.otf "/home/$username/.fonts/"
+sudo chown "$username:$username" "/home/$username/.fonts/"*
+
+# Reloading Font
 fc-cache -vf
+# Removing zip Files
+rm FiraCode.zip Meslo.zip
+
+# Install Nordzy cursor
+git clone https://github.com/alvatip/Nordzy-cursors
+cd Nordzy-cursors || exit
+./install.sh
+cd "$builddir" || exit
+rm -rf Nordzy-cursors
+
+# Install brave-browser
+yay -Sy brave-bin
+
+# Enable graphical login and change target from CLI to GUI
+sudo systemctl enable sddm
+sudo systemctl set-default graphical.target
+
+# Beautiful bash
+git clone https://github.com/ChrisTitusTech/mybash
+cd mybash || exit
+bash setup.sh
+cd "$builddir" || exit
+
+# Polybar configuration
+bash scripts/changeinterface
+
+# Use yay
+bash scripts/useyay
